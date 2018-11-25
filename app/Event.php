cb1b53dd0dc550659778a0c2ab902e5dd32a573c
pulinau <pulinau@gmail.com>
2018-11-25 12:30:55 +0530

    Create event model

---
+++ app/Event.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    protected $fillable = [
+        'event_name',
+        'event_date',
+        'description',
+        'max_volunteers'
+    ];
+}
