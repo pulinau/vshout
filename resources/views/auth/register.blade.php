81b23b87ed2e5165890199d06db3ffdcf9f619e1
prasad kavinda <pp.kavinda@gmail.com>
2018-11-25 21:53:41 +0530

    fix login/register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,7 +38,22 @@
                                 @endif
                             </div>
                         </div>
+                        <div class="form-group{{ $errors->has('role') ? ' has-error' : '' }}">
+                            <label for="role" class="col-md-4 control-label">Role</label>
 
+                            <div class="col-md-6">
+                                <select class="form-control" name="role" id="role">
+                                    <option>Please select...</option>
+                                    <option value="1">Host</option>
+                                    <option value="2">Volunteer</option>
+                                </select>
+                                @if ($errors->has('role'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('role') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
 
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
 
 Route::resource('events', 'EventController');
 
-// Auth::routes();
+Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/home', 'HomeController@index')->name('home');
