dc88d9f34c993a14fa6ab798533da96a8a7467c8
pulinau <pulinau@gmail.com>
2018-11-25 14:37:58 +0530

    Add view events table

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -14,7 +14,9 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        $events = Event::all();
+
+        return view('manageEvents.index', compact('events'));
     }
 
     /**
