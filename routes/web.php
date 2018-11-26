43e08214af575c8cf069a9d3bceedd7232b533f7
pulinau <pulinau@gmail.com>
2018-11-26 16:51:42 +0530

    Add auth module

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ Route::get('/', function () {
 Route::resource('events', 'EventController');
 Route::resource('feedback', 'FeedbackController');
 
+Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
