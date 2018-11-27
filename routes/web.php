fb87d4b7ac67f9d4198e840dbc90384fd6c74de5
SahanBuddhika <sahan.bu.1994@gmail.com>
2018-11-27 07:27:28 +0530

    add template

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Auth::routes();
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/homepage', function () {
+    return view('homepage');
+});
